84ea2863d51cffbee2b68d4e5b1ccf1b20d85347
jhe <1401448923@qq.com>
2026-06-02 12:50:21 +0000

    revise script

--- throughput_eval/all_run.sh
+++ throughput_eval/all_run.sh
@@ -18,3 +18,9 @@ NSYS_PROFILE=0 CACHE=1 CACHE_POLICY=LRU bash ./run_n2n_qwen_retroinfer_seqlen.sh
 NSYS_PROFILE=0 CACHE=3 CACHE_POLICY=LRU bash ./run_n2n_qwen_retroinfer_seqlen.sh
 NSYS_PROFILE=1 CACHE=1 CACHE_POLICY=LRU bash ./run_n2n_qwen_retroinfer_seqlen.sh
 NSYS_PROFILE=1 CACHE=3 CACHE_POLICY=LRU bash ./run_n2n_qwen_retroinfer_seqlen.sh
+
+
+NSYS_PROFILE=0 CACHE=1 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_batchsize.sh
+NSYS_PROFILE=0 CACHE=3 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_batchsize.sh
+NSYS_PROFILE=0 CACHE=1 CACHE_POLICY=LRU bash ./run_n2n_qwen_retroinfer_batchsize.sh
+NSYS_PROFILE=0 CACHE=3 CACHE_POLICY=LRU bash ./run_n2n_qwen_retroinfer_batchsize.sh
