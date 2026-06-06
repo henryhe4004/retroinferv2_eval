8dc2feeba41275c1f3f12d99ae020b4be6a5ba1a
jhe <1401448923@qq.com>
2026-06-06 13:49:30 +0000

    update all_run.sh

--- throughput_eval/all_run.sh
+++ throughput_eval/all_run.sh
@@ -7,7 +7,7 @@
 export TOPK="${TOPK:-0.10}"
 export RETRIEVAL_BUDGET="${RETRIEVAL_BUDGET:-${TOPK}}"
 export ESTIMATION_BUDGET="${ESTIMATION_BUDGET:-0.232}"
-export RETROINFER_CORE="${RETROINFER_CORE:-18}"
+export RETROINFER_CORE="${RETROINFER_CORE:-22}"
 
 run_experiment() {
     local name="$1"
@@ -17,29 +17,36 @@ run_experiment() {
     "$@" || echo "[WARN] experiment failed, continue: ${name}"
 }
 
-# llama Cache=1 用作bg 
-run_experiment "llama seqlen nsys0 cache1 LRU" env NSYS_PROFILE=0 CACHE=1 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
-run_experiment "llama seqlen nsys1 cache1 LRU" env NSYS_PROFILE=1 CACHE=1 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
+# # llama Cache=1 用作bg 
+# run_experiment "llama seqlen nsys0 cache1 LRU" env NSYS_PROFILE=0 CACHE=1 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
+# run_experiment "llama seqlen nsys1 cache1 LRU" env NSYS_PROFILE=1 CACHE=1 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
 
-run_experiment "llama seqlen nsys0 cache1 FIFO" env NSYS_PROFILE=0 CACHE=1 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=FIFO bash ./run_n2n_llama_retroinfer_seqlen.sh
-run_experiment "llama seqlen nsys1 cache1 FIFO" env NSYS_PROFILE=1 CACHE=1 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=FIFO bash ./run_n2n_llama_retroinfer_seqlen.sh
+# run_experiment "llama seqlen nsys0 cache1 FIFO" env NSYS_PROFILE=0 CACHE=1 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=FIFO bash ./run_n2n_llama_retroinfer_seqlen.sh
+# run_experiment "llama seqlen nsys1 cache1 FIFO" env NSYS_PROFILE=1 CACHE=1 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=FIFO bash ./run_n2n_llama_retroinfer_seqlen.sh
 
-
-
-#llama用作eval seq 需要nsys LRU
+# llama Cache=3 用作bg
 run_experiment "llama seqlen nsys0 cache3 LRU" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
-run_experiment "llama seqlen nsys0 cache3 LRU" env NSYS_PROFILE=1 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
+run_experiment "llama seqlen nsys1 cache3 LRU" env NSYS_PROFILE=1 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
 
-
-
-#qwen用作eval seq 需要nsys LRU
-run_experiment "qwen seqlen nsys0 cache3 LRU" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU  bash ./run_n2n_qwen_retroinfer_seqlen.sh
-run_experiment "qwen seqlen nsys1 cache3 LRU" env NSYS_PROFILE=1 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU  bash ./run_n2n_qwen_retroinfer_seqlen.sh
+run_experiment "llama seqlen nsys0 cache3 FIFO" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=FIFO bash ./run_n2n_llama_retroinfer_seqlen.sh
+run_experiment "llama seqlen nsys1 cache3 FIFO" env NSYS_PROFILE=1 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=FIFO bash ./run_n2n_llama_retroinfer_seqlen.sh
 
 
 
 
-#llama用作eval batchsize 不需要nsys
-run_experiment "llama batchsize nsys0 cache3 LRU" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU  bash ./run_n2n_llama_retroinfer_batchsize.sh
-#qwen用作eval batchsize 不需要nsys
-run_experiment "qwen batchsize nsys0 cache3 LRU" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU  bash ./run_n2n_qwen_retroinfer_batchsize.sh
+# #llama用作eval seq 需要nsys LRU
+# run_experiment "llama seqlen nsys0 cache3 LRU" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
+# run_experiment "llama seqlen nsys0 cache3 LRU" env NSYS_PROFILE=1 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU bash ./run_n2n_llama_retroinfer_seqlen.sh
+
+
+# #qwen用作eval seq 需要nsys LRU
+# run_experiment "qwen seqlen nsys0 cache3 LRU" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU  bash ./run_n2n_qwen_retroinfer_seqlen.sh
+# run_experiment "qwen seqlen nsys1 cache3 LRU" env NSYS_PROFILE=1 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU  bash ./run_n2n_qwen_retroinfer_seqlen.sh
+
+
+
+
+# #llama用作eval batchsize 不需要nsys
+# run_experiment "llama batchsize nsys0 cache3 LRU" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU  bash ./run_n2n_llama_retroinfer_batchsize.sh
+# #qwen用作eval batchsize 不需要nsys
+# run_experiment "qwen batchsize nsys0 cache3 LRU" env NSYS_PROFILE=0 CACHE=3 RETROINFER_CORE="${RETROINFER_CORE}" RETRO_CACHE_STATS=0 GPU_MEM_MONITOR_INTERVAL_SEC=999999 CACHE_POLICY=LRU  bash ./run_n2n_qwen_retroinfer_batchsize.sh
